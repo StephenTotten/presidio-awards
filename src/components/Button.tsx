b3fdd94ed0a37e8e59f276824e33b37d0ef0d6a2
Stephen Totten <stephentotten@presidio.com>
2023-12-12 14:22:10 +0000

    Squashed commit of the following
    commit e652b276 
    Author: stephentotten <stephentotten@presidio.com> 
    Date: Mon Dec 11 2023 16:39:19 GMT-0500 (Eastern Standard Time) 
    
        landing centered appropriately
    
    
    commit 6fb25f95 
    Author: stephentotten <stephentotten@presidio.com> 
    Date: Mon Dec 11 2023 16:25:38 GMT-0500 (Eastern Standard Time) 
    
        Basic welcome & button
    
    
    commit 1b5cc0e5 
    Author: stephentotten <stephentotten@presidio.com> 
    Date: Mon Dec 11 2023 15:44:26 GMT-0500 (Eastern Standard Time) 
    
        Folder structure and landing skeleton

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+const Button = () => {
+    return (
+        <div className="container">
+            <div>
+                <button className="shrink-border">Log In</button>
+            </div>
+        </div>
+    )
+}
+
+export default Button;
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import Button from "../components/button";
+
+const Landing = () => {
+    return (
+        <div className="center">
+          <h2>Welcome to</h2>
+          <h1>Presidio Awards</h1>
+          <Button />
+        </div>
+      );
+}
+
+export default Landing;
