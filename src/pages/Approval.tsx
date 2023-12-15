bd32b9ef8d5428e0fd1c020d74aa6df734c14474
stephentotten <stephentotten@presidio.com>
2023-12-15 15:10:18 -0600

    Modal now with approve/reject

--- src/pages/Approval.tsx
+++ src/pages/Approval.tsx
@@ -1,17 +1,30 @@
-import Button from "../components/Button";
+import { useState } from 'react';
+import Button from '../components/Button';
+import RejectionModal from '../components/RejectionModal';
 
 const Approval = () => {
-    return (
-        <>
-        <h1 className="nomTitle">Nominations</h1>
-        <div className="nominations">
-            <h2>Nick Truong</h2>
-            <Button text="Approve" />
-            <Button text="Reject" 
-            id="rejectButton"/>
-        </div>
-        </>
-    );
-}
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleReject = (reason: string) => {
+    console.log('Reason for rejection:', reason);
+  };
+
+  return (
+    <>
+      <h1 className="nomTitle">Nominations</h1>
+      <div className="nominations">
+        <h2>Nick Truong: Spot Award</h2>
+        <Button text="Approve" />
+        <Button text="Reject" id="rejectButton" onClick={() => setModalOpen(true)} />
+      </div>
+
+      <RejectionModal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        onReject={handleReject}
+      />
+    </>
+  );
+};
 
 export default Approval;
