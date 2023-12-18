c9b3a71cb5e6772da40f12cf9ac8aef413c6d9b2
stephentotten <stephentotten@presidio.com>
2023-12-18 16:39:01 -0600

    Navbar pathing & tweaks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Button from '../components/Button';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <Button text="Dashboard"/>
-      <Button text="Nominate"/>
+      <Link to="/approval">
+        <Button text="Approve/Reject"/>
+      </Link>
+      <Link to="/nomination">
+        <Button text="Nominate"/>
+      </Link>
       <Button text="Logout" id="logoutButton"/>
 
     </nav>
