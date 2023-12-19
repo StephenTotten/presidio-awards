7d30e67fca0db1178c1a4b98436cddbf1dd41ea8
stephentotten <stephentotten@presidio.com>
2023-12-19 13:18:49 -0600

    Nomination form added to

--- src/pages/Nomination.tsx
+++ src/pages/Nomination.tsx
@@ -6,18 +6,37 @@ const Nomination: React.FC = () => {
     <div className="login-box">
       <h2>Nomination Form</h2>
       <form>
-        <div className="user-box">
+      <div className="user-box">
           <input type="text" name="" required={true} />
-          <label>Employee Name</label>
+          <label>Nominated By</label>
         </div>
         <div className="user-box">
           <input type="text" name="" required={true} />
-          <label>Employee Email</label>
+          <label>Nominee Name</label>
+        </div>
+        <div className="user-box">
+          <input type="text" name="" required={true} />
+          <label>Nominee Email</label>
         </div>
         <div className="user-box">
           <select name="award" required={true}>
             <option value="" disabled selected>
-              Select an Award +
+              Nominee's Department
+            </option>
+            <option value="appdev">AppDev</option>
+            <option value="devops">DevOps</option>
+            <option value="cloud-migration">Cloud Migration</option>
+            <option value="data-analytics">Data and Analytics</option>
+            <option value="qa">QA</option>
+            <option value="pmo">PMO</option>
+            <option value="cloud-managed-services">Cloud Managed Services</option>
+            <option value="g-and-a">G&A</option>
+          </select>
+        </div>
+        <div className="user-box">
+          <select name="award" required={true}>
+            <option value="" disabled selected>
+              Select an Award
             </option>
             <option value="spot">Spot</option>
             <option value="drona">Drona</option>
