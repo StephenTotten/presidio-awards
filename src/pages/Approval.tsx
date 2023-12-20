c3e0c54152b53c4373214fc09411c343216d206c
stephentotten <stephentotten@presidio.com>
2023-12-20 09:36:34 -0600

    API testing

--- src/pages/Approval.tsx
+++ src/pages/Approval.tsx
@@ -1,11 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from '../components/Button';
 import RejectionModal from '../components/RejectionModal';
 import ApprovalModal from '../components/ApprovalModal';
+import axios, { AxiosResponse } from 'axios';
+
+interface Nominee {
+  nominationId: number;
+  nominator: string;
+  nominee: string;
+  awardType: string;
+  awardId: string;
+}
 
 const Approval = () => {
   const [isRejectOpen, setRejectOpen] = useState(false);
   const [isApproveOpen, setApproveOpen] = useState(false);
+  const [nomineeData, setNomineeData] = useState<Nominee[] | null>(null);
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}nomination`)
+      .then((response: AxiosResponse<Nominee[]>) => setNomineeData(response.data))
+      .catch(error => console.error('Error fetching nominee data:', error));
+  }, []);
 
   const handleReject = (reason: string) => {
     console.log('Reason for rejection:', reason);
@@ -15,9 +31,13 @@ const Approval = () => {
     <>
       <h1 className="nomTitle">Nominations</h1>
       <div className="nominations">
-        <h2>Nick Truong: Spot Award</h2>
-        <Button text="Approve" onClick={() => setApproveOpen(true)}/>
-        <Button text="Reject" id="rejectButton" onClick={() => setRejectOpen(true)} />
+        {nomineeData && nomineeData.map(nominee => (
+          <div key={nominee.nominationId}>
+            <h2>{`${nominee.nominee}: ${nominee.awardType}, Nominated by ${nominee.nominator}`}</h2>
+            <Button text="Approve" onClick={() => setApproveOpen(true)} />
+            <Button text="Reject" id="rejectButton" onClick={() => setRejectOpen(true)} />
+          </div>
+        ))}
       </div>
 
       <RejectionModal
