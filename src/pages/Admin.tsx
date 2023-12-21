b26a16c74fe5ae98717b6c702199be6a30b64528
stephentotten <stephentotten@presidio.com>
2023-12-20 22:39:09 -0600

    winners page complete

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -2,36 +2,40 @@ import { useState } from 'react';
 import Button from '../components/Button';
 import AddRoleModal from '../components/AddRoleModal';
 import RemoveRoleModal from '../components/RemoveRoleModal';
+import { Link } from 'react-router-dom';
 
 const Admin = () => {
   const [isAddRoleModalOpen, setAddRoleModalOpen] = useState(false);
   const [isRemoveRoleModalOpen, setRemoveRoleModalOpen] = useState(false);
 
   return (
-    <div className="center">
-      <h2>Name: bob</h2>
-      <Button text="Add Role" onClick={() => setAddRoleModalOpen(true)} />
-      <Button text="Remove Role" onClick={() => setRemoveRoleModalOpen(true)} />
-      
-      <AddRoleModal
-        isOpen={isAddRoleModalOpen}
-        onClose={() => setAddRoleModalOpen(false)}
-        onRoleChange={(role: any) => {
+      <div className="center">
+        <Link to="../winners">
+          <Button text="List of Winners" id="winbutton"/>
+        </Link>
+        <h2>Name: bob</h2>
+        <Button text="Add Role" onClick={() => setAddRoleModalOpen(true)} />
+        <Button text="Remove Role" onClick={() => setRemoveRoleModalOpen(true)} />
 
-          console.log('New role selected:', role);
-          setAddRoleModalOpen(false);
-        }}
-      />
-      <RemoveRoleModal
-        isOpen={isRemoveRoleModalOpen}
-        onClose={() => setRemoveRoleModalOpen(false)}
-        onRoleChange={(role: any) => {
+        <AddRoleModal
+          isOpen={isAddRoleModalOpen}
+          onClose={() => setAddRoleModalOpen(false)}
+          onRoleChange={(role: any) => {
 
-          console.log('New role selected:', role);
-          setRemoveRoleModalOpen(false);
-        }}
-      />
-    </div>
+            console.log('New role selected:', role);
+            setAddRoleModalOpen(false);
+          }}
+        />
+        <RemoveRoleModal
+          isOpen={isRemoveRoleModalOpen}
+          onClose={() => setRemoveRoleModalOpen(false)}
+          onRoleChange={(role: any) => {
+
+            console.log('New role selected:', role);
+            setRemoveRoleModalOpen(false);
+          }}
+        />
+      </div>
   );
 }
 
