59abf535a890f177e5add2835572ecd1a445680f
stephentotten <stephentotten@presidio.com>
2023-12-22 09:43:10 -0600

    winner table sorting

--- src/pages/Winners.tsx
+++ src/pages/Winners.tsx
@@ -2,19 +2,20 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 interface Winner {
-    nominationId: number;
-    nominator: string;
-    nominee: string;
-    awardType: string;
-    awardId: string;
-  }
+  nominationId: number;
+  nominator: string;
+  nominee: string;
+  awardType: string;
+  awardId: string;
+}
 
 const Winners = () => {
-    const [winners, setWinners] = useState<Winner[]>([]);
-
+  const [winners, setWinners] = useState<Winner[]>([]);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+  const [sortColumn, setSortColumn] = useState<'nominee' | 'awardType'>('nominee');
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}winners`)
+    axios.get<Winner[]>(`${import.meta.env.VITE_BACKEND_URL}winners`)
       .then(response => {
         setWinners(response.data);
       })
@@ -23,18 +24,46 @@ const Winners = () => {
       });
   }, []);
 
+  const handleSort = (column: 'nominee' | 'awardType') => {
+    setSortColumn(column);
+    setSortOrder((prevSortOrder) => (prevSortOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const sortedWinners = [...winners].sort((a, b) => {
+    const columnA = a[sortColumn].toUpperCase();
+    const columnB = b[sortColumn].toUpperCase();
+
+    if (columnA < columnB) {
+      return sortOrder === 'asc' ? -1 : 1;
+    }
+    if (columnA > columnB) {
+      return sortOrder === 'asc' ? 1 : -1;
+    }
+    return 0;
+  });
+
   return (
-    <div className="winner">
-      <h1>List of Winners</h1>
+    <div className="center">
+      <h2>List of Winners</h2>
       <table>
         <thead>
           <tr>
-            <th>Winner</th>
-            <th>Award Won</th>
+            <th onClick={() => handleSort('nominee')}>
+              Winner
+              {sortColumn === 'nominee' && (
+                <span>{sortOrder === 'asc' ? ' ▲' : ' ▼'}</span>
+              )}
+            </th>
+            <th onClick={() => handleSort('awardType')}>
+              Award Won
+              {sortColumn === 'awardType' && (
+                <span>{sortOrder === 'asc' ? ' ▲' : ' ▼'}</span>
+              )}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {winners.map(winner => (
+          {sortedWinners.map(winner => (
             <tr key={winner.nominationId}>
               <td>{winner.nominee}</td>
               <td>{winner.awardType}</td>
