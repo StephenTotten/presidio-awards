e1da3cb1d3dffa8d3845412089093991914d92cb
stephentotten <stephentotten@presidio.com>
2023-12-22 10:38:08 -0600

    landing animation

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 const Landing = () => {
     return (
         <div className="center">
-          <h2>Welcome to</h2>
-          <h1>Presidio Awards</h1>
+          <h2 className="lineUp">Welcome to</h2>
+          <h1 className="lineUp">Presidio Awards</h1>
           <Link to ="/login">
           <Button 
           text="Log In"
+          id="lineUp"
           />
           </Link>
         </div>
